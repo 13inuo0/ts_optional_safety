f89a18c91996e9e07ab6bb42c1aea885dc299bd2
13inuo0 <13inu.o0@gmail.com>
2026-05-12 13:52:43 +0900

    인덱스 시그니처 주의점 안정성이 떨어질 수 있음

--- src/index_signature.ts
+++ src/index_signature.ts
@@ -8,3 +8,19 @@ let user: {
   gender: "male",
   address: "seoul",
 };
+
+// this 사용
+interface Person {
+  name: string;
+  printName: () => string;
+//   [key: string]: string | (() => string); //위와 같이 다 작성하게 되면 인덱스 시그니처를 사용하지 않는것과 같은 상태.
+}
+
+const person: Person = {
+  name: "John",
+  printName() {
+    return this.name; //에러 발생 가능성 -> name에 타입 지정으로 해결
+  },
+};
+
+person.printName(); //에러 발생 가능성 -> printName() 타입 지정
